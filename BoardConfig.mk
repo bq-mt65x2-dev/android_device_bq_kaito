b7649094b5adf3a42845be605c8c35a50f45ba8e
R0rt1z2 <rogerortizleal@gmail.com>
2022-08-21 16:31:12 +0300

    kaito: Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2022 The LineageOS Project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+-include device/bq/mt6592-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/bq/kaito
+
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := zImage
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/zImage
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_OFFSET = 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_SECOND_OFFSET := 0x00f00000
+BOARD_TAGS_OFFSET := 0x00000100
+TARGET_KERNEL_ARCH := arm
+BOARD_MKBOOTIMG_ARGS := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --second_offset $(BOARD_SECOND_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := kaito,Aquaris_E10_3G,alps
+
+# Partitions
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_BOOTIMAGE_PARTITION_SIZE := 20971520
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 20971520
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1283457024
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1237319680
+BOARD_CACHEIMAGE_PARTITION_SIZE := 1073741824
+
+# System Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
---
+++ lineage.mk
@@ -0,0 +1,46 @@
+#
+# Copyright 2020 The LineageOS Project.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/bq/kaito/device.mk)
+
+# Inherit from common device
+$(call inherit-product, device/bq/mt6592-common/mt6592.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := bq Aquaris E10
+PRODUCT_DEVICE := kaito
+PRODUCT_NAME := lineage_kaito
+PRODUCT_BRAND := bq
+PRODUCT_MODEL := Aquaris E10
+PRODUCT_MANUFACTURER := bq
+PRODUCT_GMS_CLIENTID_BASE := android-bq
+
+# Set product device & name
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   TARGET_DEVICE=kaito \
+   PRODUCT_NAME=kaito
+
+# Use the latest approved GMS identifiers unless running a signed build
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=bq/Aquaris_E10_3G/Aquaris_E10_3G:4.4.2/KOT49H/1436772095:user/release-keys \
+    PRIVATE_BUILD_DESC="kaito-user 4.4.2 KOT49H f0a6e09 release-keys"
