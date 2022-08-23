f4f5ddbd141593d8ff930e0fcd608c1a0f994ad7
jmpfbmx <jmpf.bmx@gmail.com>
2022-08-23 22:23:08 +0200

    kaito: proprietary-files: We aren't commonized anymore
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>
    Change-Id: I72ba3c4df31c1fa44f00af06027458cf0787d82c

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -17,11 +17,31 @@
 
 set -e
 
-# Required!
-export DEVICE=kaito
-export DEVICE_COMMON=mt6592-common
-export VENDOR=bq
+DEVICE=kaito
+VENDOR=bq
 
-export DEVICE_BRINGUP_YEAR=2022
+INITIAL_COPYRIGHT_YEAR=2022
 
-./../../$VENDOR/$DEVICE_COMMON/setup-makefiles.sh $@
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+CM_ROOT="$MY_DIR"/../../..
+
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+# Copyright headers and guards
+write_headers
+
+write_makefiles "$MY_DIR"/proprietary-files.txt
+
+# Finish
+write_footers
